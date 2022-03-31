6e0125475932725e90160f024dc510b1fed8b5cf
car2854 <car2854@gmail.com>
2022-03-31 19:21:18 -0400

    .

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,16 +5,27 @@ const { pool } = require('../database/config');
 
 const createUser = async(req, res) => {
   try {
-
+    
     const {name, email, password} = req.body;
+    
+
+    const queryTextGet = 'SELECT * FROM usuario WHERE email = $1';
+    const respGet = await pool.query(queryTextGet, [email]);
+    console.log(respGet.rowCount);
+    if (respGet.rowCount > 0){
+      return res.status(400).json({
+        ok: false,
+        msg: 'El email ya esta siendo utilizado'
+      });
+    }
 
     const salt = bcrypt.genSaltSync();
     const passwordEncry = bcrypt.hashSync(password, salt);
 
-    const queryText = 'INSERT INTO usuario(nombre, email, password) VALUES($1, $2, $3)';
+    const queryTextSave = 'INSERT INTO usuario(nombre, email, password) VALUES($1, $2, $3)';
     const values = [name, email, passwordEncry];
 
-    const resp = await pool.query(queryText, values);
+    const resp = await pool.query(queryTextSave, values);
 
     return res.json({
       ok: true,

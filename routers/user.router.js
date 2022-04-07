7c3f54d5ccc9781ca9dfd7c1933a7f5c6f71991e
car2854 <car2854@gmail.com>
2022-04-06 20:41:34 -0400

    0

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
 
 const { createUser } = require('../controllers/user.controller');
+const { validateFields } = require("../middleware/validate_fields");
 
 const app = Router();
 
-app.post('/', createUser);
+app.post('/', [
+  check('name', 'El name es obligatorio').not().isEmpty(),
+  check('email', 'El email es invalido').isEmail(),
+  check('password', 'El password es obligatorio').not().isEmpty(),
+  validateFields
+],createUser);
 
 module.exports = app;
